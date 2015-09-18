839a2f6a40e319a32085f5b791d88c2261332cf4
From Zero <jorge@fromzero.io>
2015-09-18 22:50:29 +0000

    adding app run params

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, render_template, redirect, url_for, flash
 app = Flask(__name__)
 
@@ -26,4 +28,4 @@ def valid_login(username, password):
 if __name__ == '__main__':
     app.debug = True
     app.secret_key = 'SuperSecretKey'
-    app.run()
+    app.run(app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 5000))))
