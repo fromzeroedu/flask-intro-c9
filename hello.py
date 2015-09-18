a91c720032c780842e5502f57ef5e1d41740b83c
From Zero <jorge@fromzero.io>
2015-09-18 22:43:39 +0000

    adding app run params

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, render_template, redirect, url_for
 app = Flask(__name__)
 
@@ -24,4 +26,4 @@ def valid_login(username, password):
 
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    app.run(app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 5000))))
